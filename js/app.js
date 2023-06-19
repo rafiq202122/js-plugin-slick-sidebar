6ed689de60721c91f7167af8c4b9fecaaa2d313b
rafiq202122 <127656693+rafiq202122@users.noreply.github.com>
2023-06-19 11:09:38 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+
+
+
+  $(function (){
+    $('.sliders').slick({
+        slidesToShow: 5,
+       //autoplay: true,
+       // autoplaySpeed: 2000,
+        centerMode: true,
+        centerPadding: '0px',
+        arrow: true,
+        prevArrow:'.leftArrow',
+        nextArrow:'.rightArrow',
+        dots: true,
+
+    
+       
+      });
+
+
+
+
+
+  })
